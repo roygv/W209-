6417889ae5dfac28c93d78d6ea72b8cde645fe5c
gderoo <guillaume.de.roo@gmail.com>
2017-04-02 12:48:55 -0700

    clean up 2

---
+++ FinalProject/Site_bars/notificationModule.js
@@ -0,0 +1,53 @@
+///////  Notification Center ///////
+
+var node = null;
+
+document.getElementById('node').innerHTML = node;
+
+function render_notification(v, d) {
+    return v + "<div onclick='notifClick(this);' id='"+ d.nodeID +"' class='delay notibox'><b>"+ (d.date.toTimeString().split(' ')[0]) +"</b> (Notification #"+ d.alertID +") <br />\
+    "+ d.nature + " at node "+ d.nodeID +" <br /> <br /> \
+        <div class='delay cancel'>Dismiss</div> \
+        <div class='delay solved'>Resolved</div> \
+        <div class='delay escalate'>Escalate</div> \
+    </div>"
+}
+
+d3.csv("data/alert_data.csv", type_alert, function(error, data) {
+       if (error) throw error;
+       
+       var n = data.length;
+       var notif_text = "";
+       
+       for (var i = 0; i < data.length; i++) {
+       var notif_text = render_notification(notif_text, data[i]);
+       }
+       
+       $('#notificationCenter').html(notif_text);
+       
+});
+
+function type_alert(d) {
+    d.date = new Date(d.date);
+    d.alertID = +d.alertID;
+    d.nodeID = +d.nodeID;
+    return d;
+}
+
+function notifClick(d){
+    var node = d.id;
+    document.getElementById('node').innerHTML = node;
+}
+
+
+$(".toggle, .sidetoggle").click(function () {
+                   $(".sidebar").toggleClass('active');
+                   });
+
+$(document).on("click",".cancel, .solved", function () {
+                   $(this).parent().toggleClass('gone');
+                   });
+
+$(document).on("click",".escalate", function () {
+                   $(this).parent().toggleClass('escalated');
+                   });
--- FinalProject/Site_bars/topbarModule.js
+++ FinalProject/Site_bars/topbarModule.js
@@ -1,12 +1,3 @@
-
-/////// Functions to open and close the notification center ///////
-
-function openMenu() {
-    document.getElementById("rightMenu").style.display = "block";}
-function closeMenu() {
-    document.getElementById("rightMenu").style.display = "none";}
-
-
 ///////  Get time to display in the top bar ///////
 
 var current_date = new Date();
@@ -120,58 +111,3 @@ function type_timeseries(d) {
     d.events = +d.events;
     return d;
 }
-
-///////  Notification Center ///////
-
-
-var node = null;
-
-document.getElementById('node').innerHTML = node;
-
-function render_notification(v, d) {
-    return v + "<div onclick='notifClick(this);' id='"+ d.nodeID +"' class='delay notibox'><b>"+ (d.date.toTimeString().split(' ')[0]) +"</b> (Notification #"+ d.alertID +") <br />\
-    "+ d.nature + " at node "+ d.nodeID +" <br /> <br /> \
-        <div class='delay cancel'>Dismiss</div> \
-        <div class='delay solved'>Resolved</div> \
-        <div class='delay escalate'>Escalate</div> \
-    </div>"
-}
-
-d3.csv("data/alert_data.csv", type_alert, function(error, data) {
-       if (error) throw error;
-       
-       var n = data.length;
-       var notif_text = "";
-       
-       for (var i = 0; i < data.length; i++) {
-       var notif_text = render_notification(notif_text, data[i]);
-       }
-       
-       $('#notificationCenter').html(notif_text);
-       
-});
-
-function type_alert(d) {
-    d.date = new Date(d.date);
-    d.alertID = +d.alertID;
-    d.nodeID = +d.nodeID;
-    return d;
-}
-
-function notifClick(d){
-    var node = d.id;
-    document.getElementById('node').innerHTML = node;
-}
-
-
-$(".toggle, .sidetoggle").click(function () {
-                   $(".sidebar").toggleClass('active');
-                   });
-
-$(document).on("click",".cancel, .solved", function () {
-                   $(this).parent().toggleClass('gone');
-                   });
-
-$(document).on("click",".escalate", function () {
-                   $(this).parent().toggleClass('escalated');
-                   });
